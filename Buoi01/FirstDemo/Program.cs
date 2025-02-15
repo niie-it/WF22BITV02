563cf51e507d4fe3559a9d298e43147df58b1b44
Hien Luong Tran Hy <hyhien@gmail.com>
2025-02-15 07:05:34 +0700

    First commit

---
+++ Buoi01/FirstDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Hello, World!");
+string HoTen;
+int NamSinh;
+Console.Write("Nhập họ tên: ");
+HoTen = Console.ReadLine();
+Console.Write("Nhập năm sinh: ");
+NamSinh = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Bạn {HoTen}, sinh {NamSinh}, {DateTime.Now.Year - NamSinh} tuổi");
