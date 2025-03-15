e49495d13c727949b259d0b7d7be5a406651294c
Hien Luong Tran Hy <hyhien@gmail.com>
2025-03-15 09:36:49 +0700

    Demo connect DB

---
+++ Buoi12/DemoCSDL/DemoCSDL/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DemoCSDL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string chuoiKetNoi = "Data Source=.;Initial Catalog=QLHocSinh;Integrated Security=True;";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lấy dữ liệu đổ vô DataGridView
+            var conn = new SqlConnection(chuoiKetNoi);
+            var query = "SELECT * FROM HocSinh";
+            var da = new SqlDataAdapter(query, conn);
+            var dt = new DataTable();
+            da.Fill(dt);
+
+
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
